8a3f647af24e82dbfb4c7ac491cd5d7e0f864f64
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-10-03 15:35:53 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,6 @@
+const URL = 'mongodb+srv://Onkar:F5Z6B0KarUxuqzhJ@cluster0.roxwh.mongodb.net';
+const dbName = 'zen-class-student-dashboard';
+
+module.exports = {
+    dbConfig: `${URL}/${dbName}`
+};
